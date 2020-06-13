586569600052c706866bf9629cd8be1dfdb22ea6
VitalR <vitalii.rodikov@gmail.com>
2020-06-13 14:52:06 +0300

    initial commit

---
+++ flaskblogapp/__init__.py
@@ -0,0 +1,43 @@
+import os
+# import flask_whooshalchemy
+# import flask_whooshalchemyplus
+# from flask.ext.whooshalchemy import Whooshee
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app)  # SQL instance
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+ADMIN = os.environ.get('EMAIL_USER')
+
+# app.config['WHOOSH_BASE'] = 'whoosh'
+# whooshee = Whooshee(app)
+
+
+from flaskblogapp import routes
+from flaskblogapp import errors_handler
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+
+    file_handler = RotatingFileHandler('flask_blog_app.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Flask-Blog-App startup')
---
+++ flaskblogapp/errors_handler.py
@@ -0,0 +1,17 @@
+from flaskblogapp import app
+from flask import render_template, request
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('500.html'), 500
